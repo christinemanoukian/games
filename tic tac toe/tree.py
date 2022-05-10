cd4bdbf579c0d053adbccf53bf55ca7fa3c1c2e9
christinemanoukian <christinemanoukian18@gmail.com>
2022-05-10 04:51:22 +0000

    pls help with tree

--- tic tac toe/tree.py
+++ tic tac toe/tree.py
@@ -42,6 +42,7 @@ class Node:
 class TicTacToeTree:
     def __init__(self):
         self.root_node = Node([[0 for j in range(3)] for i in range(3)])
+        self.leaf_node_count = 0
 
 
     def build_tree(self):
@@ -50,46 +51,52 @@ class TicTacToeTree:
         visited = [root_node]
         while queue != []:
             current_node = queue[0]
+            #print(current_node)
             if current_node.winner is None:
                 current_board_state = current_node.state
+                #print(current_board_state)
                 player = current_node.player
-                open_spaces = current_board_state.check_for_open_spaces()
+                open_spaces = self.check_for_open_spaces(current_board_state)
+                print(open_spaces)
                 for possible_move in open_spaces:
                     new_current_board_state = list(current_board_state)
-                    new_current_board_state[possible_move] = player
+                    new_current_board_state[possible_move[0]][possible_move[1]] = player
+                    print(new_current_board_state)
                     new_node = Node(new_current_board_state)
                     current_node.children.append(new_node)
+                    #print(current_node.children)
                     new_node.parent = current_node
                     if new_node not in visited:
                         queue.append(new_node)
                     visited.append(new_node)
+            self.leaf_node_count += len(current_node.children)
             queue.remove(current_node)
 
 
-
     
-    def check_for_open_spaces(self):
+    def check_for_open_spaces(self, state):
         open_spaces = []
-        for i in range(len(self.state)):
-            for j in range(len(self.state[i])):
-                if self.state[i][j] == 0:
+        for i in range(len(state)):
+            for j in range(len(state[i])):
+                if state[i][j] == 0:
                     open_spaces.append((i,j))
         return open_spaces
 
     
 tree = TicTacToeTree()
 tree.build_tree()
-leaf_node_count = 0
-root_node = tree.root_node
-queue = [root_node]
-visited = [root_node]
-while queue != []:
-    current_node = queue[0]
-    for child_node in current_node.children:
-        if child_node.children == []:
-            leaf_node_count += 1
-        if child_node not in visited:
-            queue.append(child_node)
-            visited.append(child_node)
-    queue.remove(current_node)
-print(leaf_node_count)
+print(tree.leaf_node_count)
+# leaf_node_count = 0
+# root_node = tree.root_node
+# queue = [root_node]
+# visited = [root_node]
+# while queue != []:
+#     current_node = queue[0]
+#     for child_node in current_node.children:
+#         if child_node.children == []:
+#             leaf_node_count += 1
+#         if child_node not in visited:
+#             queue.append(child_node)
+#             visited.append(child_node)
+#     queue.remove(current_node)
+# print(leaf_node_count)
