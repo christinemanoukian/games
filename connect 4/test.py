7a1de4225cbf016f2c3eef6a5aaec0afd08b5ac4
christinemanoukian <christinemanoukian18@gmail.com>
2022-05-15 03:55:01 +0000

    did some stuff to strat

--- connect 4/test.py
+++ connect 4/test.py
@@ -1,14 +1,16 @@
 import sys
 sys.path.append('connect 4')
 from game import *
-from player import *
+from random_player import *
+from custom_player import *
 from strategies import *
 
 
+
 wins = {1: 0, 2: 0, 'ties': 0}
-for i in range(1000):
+for i in range(100):
     player1 = RandomPlayer()
-    player2 = RandomPlayer()
+    player2 = CustomPlayer()
     game = Game(player1, player2, log=False)
     game.run()
     if type(game.winner) is int:
@@ -16,3 +18,4 @@ for i in range(1000):
     else:
         wins['ties'] += 1
 print(wins)
+
